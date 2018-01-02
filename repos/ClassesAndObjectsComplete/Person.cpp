0f9e50a9e8099979d7e526db77532f7242517845
Macauley Hopper <macauley.hopper@xo.com>
2018-01-02 12:45:27 -0600

    Finished the ClassesandObjects section, several files have been added

---
+++ repos/ClassesAndObjectsComplete/Person.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+#include "Person.h"
+#include <iostream>
+
+using namespace std;
+
+Person::Person(string first, string last, int arbitrary) : firstname(first), lastname(last), arbitrarynumber(arbitrary)
+{
+	cout << "Constructing " << firstname << " " << lastname << endl;
+}
+
+Person::~Person()
+{
+	cout << "Destructing " << firstname << " " << lastname << endl;
+}
--- repos/ClassesAndObjectsTest/ClassesAndObjectsTest/Person.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-
-#include "Person.h"
-
-Person::Person(std::string first, std::string last, int arbitrary) : firstname(first), lastname(last), arbitrarynumber(arbitrary)
-{
-}
