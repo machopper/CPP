c57e68a677eb3a20f179e4b031b8ee00700bd4e1
Macauley Hopper <macauley.hopper@xo.com>
2018-01-11 13:44:29 -0600

    2 more exercises covering template functions, and overload operators

---
+++ repos/Max/Max/BankAccount.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "BankAccount.h"
+
+
+BankAccount::BankAccount(void)
+{
+}
+
+
+BankAccount::~BankAccount(void)
+{
+}
---
+++ repos/Max/Max/BankAccount.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class BankAccount
+{
+public:
+	BankAccount(void);
+	~BankAccount(void);
+private:
+	int BalanceInPennies;
+	//TODO: finish writing this class
+public:
+	std::string GetHolderName() { return "Not Implemented"; }
+};
+
