20de73acbc70bde9cdc12c7d7617e54145cc1b1a
Macauley Hopper <macauley.hopper@xo.com>
2018-01-31 11:07:32 -0600

    New exercise about pointers and inheritances

---
+++ repos/PointersAndInheritance/PointersAndInheritance/Tweeter.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "Tweeter.h"
+#include <iostream>
+
+
+Tweeter::Tweeter(std::string first, std::string last, int arbitrary, std::string handle): Person(first, last, arbitrary), twitterhandle(handle)
+{
+}
+
+
+Tweeter::~Tweeter()
+{
+}
+
+std::string Tweeter::GetName() const
+{
+	return Person::GetName() + " " + twitterhandle;
+}
