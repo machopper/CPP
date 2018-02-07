851c141a592760669bc3358e5c1ee43090cee727
Macauley Hopper <mac_hopper@mac.com>
2018-02-06 22:20:14 -0600

    Updated Casting exercise

---
+++ repos/Casting/Casting/Person.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+#include "Person.h"
+#include <iostream>
+
+using namespace std;
+
+Person::Person(string first, string last, int arbitrary) : firstname(first), lastname(last), arbitrarynumber(arbitrary)
+{
+}
+
+Person::~Person()
+{
+}
+
+string Person::GetName() const
+{
+	return firstname + " " + lastname;
+}
---
+++ repos/Casting/Casting/Resource.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "Resource.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+
+Resource::Resource(string n):name(n)
+{
+}
+
+
+Resource::~Resource(void)
+{
+}
